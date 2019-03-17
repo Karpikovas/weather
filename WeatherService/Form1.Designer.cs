148eba7844d08b9a5057ed8dfefd7a04f460ceed
karpikovasophie <karpikovasophie@gmail.com>
2019-03-17 20:35:35 +0300

    All changes

---
+++ WeatherService/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WeatherService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtWeather = new System.Windows.Forms.TextBox();
+            this.btnGetWeather = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCity
+            // 
+            this.txtCity.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtCity.Location = new System.Drawing.Point(32, 51);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(243, 32);
+            this.txtCity.TabIndex = 0;
+            // 
+            // txtWeather
+            // 
+            this.txtWeather.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtWeather.Location = new System.Drawing.Point(32, 96);
+            this.txtWeather.Multiline = true;
+            this.txtWeather.Name = "txtWeather";
+            this.txtWeather.Size = new System.Drawing.Size(471, 203);
+            this.txtWeather.TabIndex = 1;
+            // 
+            // btnGetWeather
+            // 
+            this.btnGetWeather.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnGetWeather.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGetWeather.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGetWeather.Location = new System.Drawing.Point(281, 24);
+            this.btnGetWeather.Name = "btnGetWeather";
+            this.btnGetWeather.Size = new System.Drawing.Size(222, 59);
+            this.btnGetWeather.TabIndex = 2;
+            this.btnGetWeather.Text = "Показать погоду";
+            this.btnGetWeather.UseVisualStyleBackColor = false;
+            this.btnGetWeather.Click += new System.EventHandler(this.btnGetWeather_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Nova", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(33, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите название города";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(526, 319);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGetWeather);
+            this.Controls.Add(this.txtWeather);
+            this.Controls.Add(this.txtCity);
+            this.Name = "Form1";
+            this.Text = "Погода";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtWeather;
+        private System.Windows.Forms.Button btnGetWeather;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WeatherService/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetWeather_Click(object sender, EventArgs e)
+        {
+            string key = "79214423-acb6-4c5f-99e0-3f0b5db84b86";
+            WeatherLocations.LocationsSoapClient locClient = new WeatherLocations.LocationsSoapClient();
+            WeatherLocations.LocationInfoFullResult response = locClient.FindByNameFull(key, txtCity.Text, 1, "RU");
+            try
+            {
+                int cityId = response.data[0].id;
+
+                WeatherData.WeatherSoapClient client = new WeatherData.WeatherSoapClient();
+                WeatherData.HHForecastResult weather = client.GetHHForecast(key, cityId);
+                txtWeather.Text = "Температура днём: " + string.Format("{0:0.#}", weather.data[0].tod) + "°С" + Environment.NewLine;
+                txtWeather.Text += "Температура ночью: " + string.Format("{0:0.#}", weather.data[0].t) + "°С" + Environment.NewLine;
+                txtWeather.Text += "Давление: " + string.Format("{0:0}", weather.data[0].p) + Environment.NewLine;
+                txtWeather.Text += "Влажность: " + weather.data[0].humidity.ToString();
+
+            } catch (Exception ex) {
+                txtWeather.Text = "Город не найден";
+            }
+        }
+    }
+}
